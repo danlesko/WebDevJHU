1e7a85de9ad3d1493a3e7cbb255ecff3e9b83a26
dalesko <dalesko@cisco.com>
2020-11-06 12:20:16 -0500

    start from scratch part 6

--- HW10/src/com/dalesko/hw10/BHCController.java
+++ HW10/src/com/dalesko/hw10/BHCController.java
@@ -23,132 +23,152 @@ import javax.servlet.http.HttpSession;
 import com.dalesko.hw10.Rates.HIKE;
 
 
+
 /**
  *
  * @author dalesko
  */
 @WebServlet("/BHCController")	
 public class BHCController extends HttpServlet {
-    /** 
+    public static final String HIKE = null;
+    public static final String STARTDATE = null;
+    public static final String DURATION = null;
+    public static final String PEOPLE = null;
+    
+	private static final long serialVersionUID = 1L;
+
+	/** 
     * Processes requests for  HTTP <code>GET</code> method.
     * @param request servlet request
     * @param response servlet response
     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+//        response.setContentType("text/html;charset=UTF-8");
+//        ServletContext servletContext = getServletContext();
+//        RequestDispatcher dispatcher = null;
+//        HikeInfo hikeInfo = new HikeInfo();
+        
+//        // Initialize errStr
+//		String errStr = "";
+//		
+//		// Initialize Date variables
+//		Date jStartDate = null;
+//		
+//		// Initialize BookingDay variables
+//		BookingDay startDay = null;
+//		BookingDay endDay = null;
+        
+		response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
         ServletContext servletContext = getServletContext();
-        RequestDispatcher dispatcher = null;
-        HikeInfo hikeInfo = new HikeInfo();
-        
-        String hike = null;
-        String startDate = null;
-        String duration = null;
-        String people = null;
-        
-        // Initialize errStr
-		String errStr = "";
-		
-		// Initialize Date variables
-		Date jStartDate = null;
-		
-		// Initialize BookingDay variables
-		BookingDay startDay = null;
-		BookingDay endDay = null;
-        
+        HikeInfo hikeInfo = (HikeInfo) session.getAttribute(HIKE);
+        if (hikeInfo == null) {
+            hikeInfo = new HikeInfo();
+            session.setAttribute(HIKE, hikeInfo);
+            RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/bhc.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            String name = request.getParameter(HIKE);
+            String password = request.getParameter(STARTDATE);
+            hikeInfo.setHike(name);
+            hikeInfo.setStartDate(password);
+            RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/results.jsp");
+            dispatcher.forward(request, response);
+        }
      	
         	
-        hike = request.getParameter("hike");
-        if (hike == null) {
-        	hike = "1";
-        }
-        startDate = request.getParameter("startDate");
-        if (startDate == null) {
-        	startDate = "2020-04-20";
-        }
-        duration = request.getParameter("duration");
-        if (duration == null) {
-        	duration = "3";
-        }
-        
-        people = request.getParameter("people");
-        if (people == null) {
-        	people = "1";
-        }
-        
-        System.out.println(startDate);
-        
-        try {
-        	jStartDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
-        } catch (ParseException e) {
-        	errStr = errStr.concat("Error parsing date! Please enter date in yyyy-MM-dd format if your browser does not support the input date type." + "\n");
-        	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-		} 
-        
-        if(jStartDate != null) {
-    		Calendar calStartDate = toCalendar(jStartDate);
-    		calStartDate.add(Calendar.DAY_OF_MONTH, Integer.parseInt(duration));
-    		Date endDate = calStartDate.getTime();
-    		Date currentD = new Date();
-    		Calendar currentDate = toCalendar(currentD);
-    		int isFuture = calStartDate.compareTo(currentDate);
-    		
-    		// Parse the start and end date fields
-    		try {
-    			startDay = new BookingDay(jStartDate.getYear()+1900,jStartDate.getMonth()+1,jStartDate.getDate());
-    		} catch (NullPointerException e1) {
-    			errStr = errStr.concat("Error in start day" + "\n");
-    			dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-    		}
-    		
-    		try {
-    			endDay = new BookingDay(endDate.getYear()+1900,endDate.getMonth()+1,endDate.getDate());
-    		} catch (NullPointerException e2) {
-    			errStr = errStr.concat("Error in End Day: "+ "\n");
-    			dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-    		}
-    	    
-    	    // Create a new Rates object and instantiate it with the type of hike chosen
-    	    Rates rates = new Rates(HIKE.values()[Integer.parseInt(hike)-1]);
-    		rates.setBeginDate(startDay);
-    		rates.setEndDate(endDay);
-            boolean success = rates.setDuration(Integer.parseInt(duration));
-            
-            // If not valid dates, show details 
-            if(isFuture != 1) {
-            	errStr = errStr.concat(
-    				    "The start date is not in the future! \n");
-            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-            }
-            else if(jStartDate.getYear()+1900 > 2025) {
-            	errStr = errStr.concat(
-    				    "You may not set a date that far in the future! (After 2025) \n");
-            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-            }
-            else if(!rates.isValidDates()) {
-            	errStr = errStr.concat(
-    				    "The dates chosen are invalid! " + rates.getDetails() + "\n");
-            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-            } 
-            // If not success, show the error dialog, otherwise append hike information to the errStr
-            else if (!success) {
-            	errStr = errStr.concat(
-    				    "Your duration is invalid!\n");
-            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
-            }
-            else {
-            	// Only show results if we have success
-            	dispatcher = servletContext.getRequestDispatcher("/results.jsp");
-    		}
-            
-            // If an error message has been set, set the bean
-            if(!errStr.equals("")) {
-            	hikeInfo.setErrStr(errStr);
-            	System.out.println(hikeInfo.getErrStr());
-            }
-        }
-        
-        dispatcher.forward(request, response);
+//        hike = request.getParameter("hike");
+//        if (hike == null) {
+//        	hike = "1";
+//        }
+//        startDate = request.getParameter("startDate");
+//        if (startDate == null) {
+//        	startDate = "2020-04-20";
+//        }
+//        duration = request.getParameter("duration");
+//        if (duration == null) {
+//        	duration = "3";
+//        }
+//        
+//        people = request.getParameter("people");
+//        if (people == null) {
+//        	people = "1";
+//        }
+//        
+//        System.out.println(startDate);
+//        
+//        try {
+//        	jStartDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+//        } catch (ParseException e) {
+//        	errStr = errStr.concat("Error parsing date! Please enter date in yyyy-MM-dd format if your browser does not support the input date type." + "\n");
+//        	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//		} 
+//        
+//        if(jStartDate != null) {
+//    		Calendar calStartDate = toCalendar(jStartDate);
+//    		calStartDate.add(Calendar.DAY_OF_MONTH, Integer.parseInt(duration));
+//    		Date endDate = calStartDate.getTime();
+//    		Date currentD = new Date();
+//    		Calendar currentDate = toCalendar(currentD);
+//    		int isFuture = calStartDate.compareTo(currentDate);
+//    		
+//    		// Parse the start and end date fields
+//    		try {
+//    			startDay = new BookingDay(jStartDate.getYear()+1900,jStartDate.getMonth()+1,jStartDate.getDate());
+//    		} catch (NullPointerException e1) {
+//    			errStr = errStr.concat("Error in start day" + "\n");
+//    			dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//    		}
+//    		
+//    		try {
+//    			endDay = new BookingDay(endDate.getYear()+1900,endDate.getMonth()+1,endDate.getDate());
+//    		} catch (NullPointerException e2) {
+//    			errStr = errStr.concat("Error in End Day: "+ "\n");
+//    			dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//    		}
+//    	    
+//    	    // Create a new Rates object and instantiate it with the type of hike chosen
+//    	    Rates rates = new Rates(HIKE.values()[Integer.parseInt(hike)-1]);
+//    		rates.setBeginDate(startDay);
+//    		rates.setEndDate(endDay);
+//            boolean success = rates.setDuration(Integer.parseInt(duration));
+//            
+//            // If not valid dates, show details 
+//            if(isFuture != 1) {
+//            	errStr = errStr.concat(
+//    				    "The start date is not in the future! \n");
+//            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//            }
+//            else if(jStartDate.getYear()+1900 > 2025) {
+//            	errStr = errStr.concat(
+//    				    "You may not set a date that far in the future! (After 2025) \n");
+//            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//            }
+//            else if(!rates.isValidDates()) {
+//            	errStr = errStr.concat(
+//    				    "The dates chosen are invalid! " + rates.getDetails() + "\n");
+//            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//            } 
+//            // If not success, show the error dialog, otherwise append hike information to the errStr
+//            else if (!success) {
+//            	errStr = errStr.concat(
+//    				    "Your duration is invalid!\n");
+//            	dispatcher = servletContext.getRequestDispatcher("/error.jsp");
+//            }
+//            else {
+//            	// Only show results if we have success
+//            	dispatcher = servletContext.getRequestDispatcher("/results.jsp");
+//    		}
+//            
+//            // If an error message has been set, set the bean
+//            if(!errStr.equals("")) {
+//            	hikeInfo.setErrStr(errStr);
+//            	System.out.println(hikeInfo.getErrStr());
+//            }
+//        }
+//        
+//        dispatcher.forward(request, response);
     } 
 
     /** 
---
+++ KMS/WebContent/welcome.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : menu
+    Created on : Dec 10, 2007, 8:48:11 PM
+	Updated on : July 12, 2019
+--%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<meta charset="ISO-8859-1">
+        <title>Succesful login!</title>
+        <jsp:useBean id="login" class="edu.rfs.Login" scope="session" />        
+    </head>
+    <body>
+        <h2>You are logged in as user <jsp:getProperty name="login" property="name" /></h2>
+    </body>
+</html>
